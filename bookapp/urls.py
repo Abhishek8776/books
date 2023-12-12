4b63213a9a35ca88c3c4ecc3e6eca157ab15b849
Abhishek P <abhishekpclt1999@gmail.com>
2023-12-12 20:52:36 +0530

    bug fix

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", BooksView.as_view(), name="book"),
-    path("<uuid:id>/", BooksView.as_view(), name="book"),
+    # path("<uuid:id>/", BooksView.as_view(), name="book"),
     # path("editbook/<uuid:id>/",name="editbook"),
     path("bookdetails/<uuid:id>/", BookDetailsView.as_view(), name="bookdetails"),
     path("deletebook/<uuid:id>/", BookDeleteView.as_view(), name="deletebook"),
